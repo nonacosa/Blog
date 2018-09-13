69ba13fd824ba8147093f47d69afbd56aa2423a7
six.nonacosa <pkwenda@163.com>
2018-09-13 18:21:09 +0800

    vue-cli-2 教程

--- code/Guava/src/main/java/com/guava/common/base/Strings_.java
+++ code/Guava/src/main/java/com/guava/common/base/Strings_.java
@@ -1,6 +1,7 @@
 package com.guava.common.base;
 
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Strings;
 
 /**
@@ -15,6 +16,12 @@ public class Strings_ {
     System.out.println(Strings.nullToEmpty(null));
     System.out.println(Strings.isNullOrEmpty(null));
     System.out.println(Strings.isNullOrEmpty(""));
+
+    System.out.println(Strings.padStart("7",10,'0'));
+    System.out.println(Strings.padEnd("7",10,'0'));
+    System.out.println(Strings.padStart("2004",3,'0'));
+
+    System.out.println(MoreObjects.firstNonNull(null, "1"));
   }
 
 }
