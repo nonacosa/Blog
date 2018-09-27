1f077bbc869467030ed0551ee69158ba0fffb274
six.nonacosa <pkwenda@163.com>
2018-09-27 23:49:01 +0800

    第二章 完善我们的vue项目
    第二章 完善我们的vue项目

---
+++ code/web_project_course/complete_project/new-bee/src/renderer/container/blog/index.vue
@@ -0,0 +1,37 @@
+ <template>
+  <div class="Blog">
+    <h1>{{ msg }}</h1>
+     
+     
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blog",
+  data() {
+    return {
+      msg: "Welcome to Your Blog Page"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
