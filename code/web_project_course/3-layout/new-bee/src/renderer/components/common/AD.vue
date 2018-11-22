fc78005b032c056de9d9fa4e0d2b316e72c4c0d5
six.nonacosa <pkwenda@163.com>
2018-11-22 23:21:00 +0800

    完结

---
+++ code/web_project_course/3-layout/new-bee/src/renderer/components/common/AD.vue
@@ -0,0 +1,32 @@
+
+<template>
+     
+             <div class="ad"><h1>澳门皇家赌场上线啦</h1></div>
+    
+</template>
+
+<script>
+import _ from "lodash";
+export default {
+  name: "AD",
+
+  data() {
+    return {};
+  },
+  destroyed() {},
+  mounted() {},
+  methods: {},
+  watch: {}
+};
+</script>
+ <style   scoped>
+.ad {
+  width: 150px;
+  height: 180px;
+  background-color: #ececec;
+  position: fixed;
+  right: 30px;
+  top: 80px;
+}
+</style>
+ 
